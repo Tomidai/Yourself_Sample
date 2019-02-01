42660d8f8caa81afce50bf99e7997b5e793e7724
Tomidai <dd1116k@gmail.com>
2019-02-01 13:37:46 +0900

    名前付き引数を学習しました。

--- Yourself/Program.cs
+++ Yourself/Program.cs
@@ -5,16 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Yourself {
-    static class Myclass {
-        public static double GetBmi(double weight, double height) {
-            return weight / (height * height);
+    class Person {
+        public string firstName;
+        public string lastName;
+
+        //引数greeting/titleに規定値を設定
+        public void Show(string greeting = "こんにちは", string title = "さん") {
+            Console.WriteLine($"{greeting},{this.lastName}{this.firstName}{title}");
+        }
+
+        public void DemoProc(string name = "default name", int age = 0) {
+            Console.WriteLine($"{name}{age}");
         }
     }
 
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(Myclass.GetBmi(76, 174));
-
+            var p = new Person() {
+                lastName = "山田", firstName = "taro"
+            };
+            p.DemoProc();
+            p.DemoProc("tomita", 20);
+            p.DemoProc("tomita");
+            p.DemoProc(age: 20);
+            p.DemoProc(name: "tomita");
+            p.DemoProc(name: "tomita", age: 20);
+            p.DemoProc(age: 20, name: "tomita");
+            //p.DemoProc(20);   !error
             //何かのキー入力で終了する
             Console.ReadKey();
         }
